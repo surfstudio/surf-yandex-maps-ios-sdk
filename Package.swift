77765f342d47bdc660c4bf25a4e1524e6bafd076
Alexander <chausov@surf.dev>
2023-03-28 14:53:41 +0400

    added package manifest

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+// Lite version for YandexMapsMobile
+import PackageDescription
+
+let package = Package(
+    name: "YandexMapsMobile",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "YandexMapsMobile",
+            targets: ["YandexMapsMobile"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "YandexMapsMobile",
+            url: "https://github.com/surfstudio/surf-yandex-maps-ios-sdk/releases/download/4.2.2/YandexMapsMobile.xcframework.zip",
+            checksum: "541882edd253171f9875d84302aa1587992ea2d816c77f259049497fa87fe2cb"
+        ),
+    ]
+)
