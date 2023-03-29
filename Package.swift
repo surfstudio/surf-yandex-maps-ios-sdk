54db76c9fa71c5c8070989cbb03474fe1277103e
Alexander <chausov@surf.dev>
2023-03-29 18:39:05 +0400

    Version bump to 4.3.1.1

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YandexMapsMobile",
-            url: "https://github.com/surfstudio/surf-yandex-maps-ios-sdk/releases/download/4.3.1/YandexMapsMobile.xcframework.zip",
+            url: "https://github.com/surfstudio/surf-yandex-maps-ios-sdk/releases/download/4.3.1.1/YandexMapsMobile.xcframework.zip",
             checksum: "c66142a783a46845301192fd7dcc5f788f7707c6344e9ed919b58a7808f498c1"
         ),
     ]
