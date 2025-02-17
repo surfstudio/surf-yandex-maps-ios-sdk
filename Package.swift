43ade225b9c27efa2c12d7182e7f18423f1338e7
Nikita Korobeinikov <nikita.korobeinikov@devexperts.com>
2025-02-17 16:28:40 +0400

    version bump to 4.10.1

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YandexMapsMobile",
-            url: "https://github.com/surfstudio/surf-yandex-maps-ios-sdk/releases/download/4.5.2/YandexMapsMobile.xcframework.zip",
-            checksum: "4e397bf3a520301a4ac743aef3d3d2c7e868c95b617e8f8a8767b0710a7fa1e7"
-        ),
+            url: "https://github.com/surfstudio/surf-yandex-maps-ios-sdk/releases/download/4.10.1/YandexMapsMobile.xcframework.zip",
+            checksum: "bd17d18812a548dfd9921487ec5ff3d326ee0a4ca6ea54cebab7478f06b85638"
+        )
     ]
 )
